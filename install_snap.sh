b4a4780fad4ee2f4cd5e3d05d2f2c13a9b8484d2
Mikel <mikel.san@educa.madrid.org>
2025-03-18 14:46:40 +0100

    arreglado

--- install_snap.sh
+++ install_snap.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #
 
-snap install -y snap-store-proxy 
-snap-proxy config set proxy.domain=172.20.0.21
 
-snap-proxy config set cache.enable=true
+# SNAP Proxy
+
+snap install snap-store-proxy 
+snap-proxy set proxy.domain=172.20.0.21
+
+snap-proxy set cache.enable=true
 # Cache size setted to 20GB
 snap-proxy config set cache.size=20480
 
 systemctl start snap-store-proxy
 systemctl enable snap-store-proxy
+
+
+# APT 
