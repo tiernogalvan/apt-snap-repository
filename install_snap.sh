970991a6574c292db453e18d7d28b5b9625042fa
Mikel <mikel.san@educa.madrid.org>
2025-03-25 14:21:31 +0100

    arreglado

--- install_snap.sh
+++ install_snap.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-#
+# Este script debe ejecutarse en una máquina virtual de Ubuntu Server (no contenedor)
 
 
 # SNAP Proxy
 
-apt update
-apt install postgresql
- sudo -u postgres psql < snap.sql
+sudo apt update
+sudo apt install postgresql
 
-snap install snap-store-proxy 
-snap-proxy set proxy.domain=172.20.0.21
+sudo -u postgres psql < snap.sql
 
-# Cache size setted to 20GB
-snap-proxy config proxy.cache.size=20480
-snap-proxy register
-
-# Domain caches.tierno.es
-# Password postgres Sandia4you
-
-# Desde los clientes
-
-
-# APT 
+sudo snap install snap-store-proxy 
+# Configurar la base de datos: password Sandia4you
+sudo snap-proxy config proxy.db.connection="postgresql://snapproxy-user@localhost:5432/snapproxy-db"
+sudo snap-proxy config proxy.domain="proxy-snap.tierno.es"
+sudo snap-proxy config proxy.cache.size=50480
+sudo snap-proxy register
