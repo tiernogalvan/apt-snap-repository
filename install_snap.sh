5da401b31c826a3c9ceac5bba60fef553418c2cb
Mikel <mikelsanvi@gmail.com>
2026-06-17 12:06:08 +0200

    actualizado install_snap con nuevo hostname

--- install_snap.sh
+++ install_snap.sh
@@ -4,14 +4,14 @@
 
 # SNAP Proxy
 
-sudo apt update
-sudo apt install postgresql
+apt update
+apt install postgresql
 
-sudo -u postgres psql < snap.sql
+su - postgres psql < snap.sql
 
-sudo snap install snap-store-proxy 
+snap install snap-store-proxy 
 # Configurar la base de datos: password Sandia4you
-sudo snap-proxy config proxy.db.connection="postgresql://snapproxy-user@localhost:5432/snapproxy-db"
-sudo snap-proxy config proxy.domain="proxy-snap.tierno.es"
-sudo snap-proxy config proxy.cache.size=50480
-sudo snap-proxy register
+snap-proxy config proxy.db.connection="postgresql://snapproxy-user@localhost:5432/snapproxy-db"
+snap-proxy config proxy.domain="snap-cache.tierno.es"
+snap-proxy config proxy.cache.size=50480
+snap-proxy register
